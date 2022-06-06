f6a8f3e9dd0eb7975a40c0b08a3f04724c9080a6
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-06 18:20:42 +0930

    add get routes for category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,36 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Category.findAll({
+    include: Product,
+  });
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categoryId = req.params.id;
+  const categories = await Category.findByPk(categoryId, {
+    include: Product,
+  });
+  res.json(categories);
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
